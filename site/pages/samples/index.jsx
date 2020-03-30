9b43235d642dd89fb0db59edb0dcf2a253660a6e
Steper Kuo <gyx8899@126.com>
2020-03-30 23:24:53 +0800

    feature(site): Add test html pages

--- site/pages/samples/index.jsx
+++ site/pages/samples/index.jsx
@@ -4,7 +4,14 @@ function Samples () {
 
 	return (
 			<div className="Sample">
-				Sample
+				<ul>
+					<li><a href='/site/pages/html/ClassName.html'>ClassName</a></li>
+					<li><a href='/site/pages/html/Event.html'>Event</a></li>
+					<li><a href='/site/pages/html/mouseTouchTrack.html'>mouseTouchTrack</a></li>
+					<li><a href='/site/pages/html/MVVM.html'>MVVM</a></li>
+					<li><a href='/site/pages/html/ObserverObject.html'>ObserverObject</a></li>
+					<li><a href='/site/pages/html/util.html'>util</a></li>
+				</ul>
 			</div>
 	);
 }
