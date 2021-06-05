780527037cc7d7e9feee35439628c252cd7c0a59
Steper Kuo <gyx8899@126.com>
2021-06-05 13:14:49 +0800

    :art: chore(Math.js): updated;

--- src/util/Math.js
+++ src/util/Math.js
@@ -1,20 +1,27 @@
-/* eslint-disable */
 export const isPrime = (number) => {
-	if (number === 0 || number === 1) {
-		return true;
-	}
-	for (let i = 2; i <= Math.sqrt(number); i++) {
-		if (number % i === 0) {
-			return false;
-		}
-	}
-	return true;
+    if (number === 0 || number === 1) {
+        return true;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
 };
 
-export const factorialOfNumber = (number) => ((number < 0)
-		? (() => {
-			throw new Error(`No negative Number: ${number}`);
-		})()
-		: (number <= 1
-				? 1
-				: number * factorialOfNumber(number - 1)));
+export const factorialOfNumber = (number) =>
+    number < 0
+        ? (() => {
+              throw new Error(`No negative Number: ${number}`);
+          })()
+        : number <= 1
+        ? 1
+        : number * factorialOfNumber(number - 1);
+
+export function hashCode(s) {
+    return s.split("").reduce(function (a, b) {
+        a = (a << 5) - a + b.charCodeAt(0);
+        return a & a;
+    }, 0);
+}
